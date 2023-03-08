e4fa94d7416c8fb1a353d6cc32f1f09b62f978e8
Batler5 <115727657+Batler5@users.noreply.github.com>
2023-03-08 13:30:12 +0300

    upper

--- 033 script.js
+++ 033 script.js
@@ -1,14 +1,14 @@
-// console.log(document.head);
-// console.log(document.documentElement);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.lastChild);
+console.log(document.head);
+console.log(document.documentElement);
+console.log(document.body.childNodes);
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
 
-// console.log(document.querySelector('#current').parentElement);
+console.log(document.querySelector('#current').parentElement);
 
-// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
+console.log(document.querySelector('[data-current="3"]').previousElementSibling);
 
-// console.log(document.body.lastElementChild);
+console.log(document.body.lastElementChild);
 
 for (let node of document.body.childNodes){
     if(node.nodeName=='#text'){
